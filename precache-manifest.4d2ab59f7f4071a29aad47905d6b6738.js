92d68e46c58adab233ed3592f6d833aae5adc31c
pf <287854@supinfo.com>
2020-07-24 18:39:15 +0200

    Updates

---
+++ precache-manifest.4d2ab59f7f4071a29aad47905d6b6738.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a5277b781fd3351c383b5ef8c05e9e",
+    "url": "/CuriculumVitae/index.html"
+  },
+  {
+    "revision": "9b3ff0b66e80b52699a2",
+    "url": "/CuriculumVitae/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "874337be2bb596be5cfb",
+    "url": "/CuriculumVitae/static/css/main.539956f6.chunk.css"
+  },
+  {
+    "revision": "9b3ff0b66e80b52699a2",
+    "url": "/CuriculumVitae/static/js/2.cf988d5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CuriculumVitae/static/js/2.cf988d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874337be2bb596be5cfb",
+    "url": "/CuriculumVitae/static/js/main.d83312fc.chunk.js"
+  },
+  {
+    "revision": "50beca51d6ec3177cce8",
+    "url": "/CuriculumVitae/static/js/runtime-main.7e13090e.js"
+  },
+  {
+    "revision": "c935cb2fea2b8d88c770394a06e06ac7",
+    "url": "/CuriculumVitae/static/media/GentiumBookBasic-Regular.c935cb2f.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/CuriculumVitae/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "a7c8304253b80b324955aa1a17044beb",
+    "url": "/CuriculumVitae/static/media/profil_picture.a7c83042.jpg"
+  }
+]);
